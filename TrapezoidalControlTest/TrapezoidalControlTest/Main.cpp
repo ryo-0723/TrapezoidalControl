2e5dee49b2c57cf5dd201fc4294faffd7caab2e8
涼 藤本 <jlgifydhdhtr@outlook.jp>
2024-05-14 09:12:19 +0900

    新しいレポジトリに移動

---
+++ TrapezoidalControlTest/TrapezoidalControlTest/Main.cpp
@@ -0,0 +1,104 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.
+#include"Trapezoidal.h"
+#include"Plotter.h"
+
+void Main()
+{
+	Vec2 point1{ 1.5,8.0 };
+	Vec2 point2{ 4.0,-9.5 };
+	Vec2 point3{ 9.0,5.0 };
+	Vec2 point4{ 10.0,0.0 };
+
+	Vec2 point{ 0,0 };
+	double startS = 0;
+	double endS = 0;
+	int next_point = 0;
+	double mil = 0;
+	Vec2 targetSpeed{ 0,0 };
+	Vec2 target{ 0,0 };
+	uint64 loop = 0;
+	Plotter plotter1, plotter2, plotter3, plotter4;
+	Trapezoidal auto_set(3.5, 5.0, { 0,0 });
+	while (System::Update())
+	{
+		ClearPrint();
+		auto_set.update(point, startS, endS);
+
+		if (Time::GetMillisec() > 3000) {
+			if (auto_set.next_status()) {
+				next_point++;
+				auto_set.set_next_status();
+			}
+		}
+
+		switch (next_point) {
+		case 1:
+			point = point1;
+			startS = 0;
+			endS = 1.2;
+			break;
+		case 2:
+			point = point2;
+			startS = 1.2;
+			endS = 0.5;
+			break;
+		case 3:
+			point = point3;
+			startS = 0.5;
+			endS = 0;
+			break;
+		case 4:
+			point = point4;
+			startS = 0;
+			endS = 0;
+			break;
+		}
+		//理想ではこの形で自動を回すこと
+
+
+		loop = Time::GetMillisec();
+		if (loop % 10) {
+			targetSpeed.y = (auto_set.getTargetPos().y - target.y) / (((double)loop * 0.001) - mil);//m/s
+			targetSpeed.x = (auto_set.getTargetPos().x - target.x) / (((double)loop * 0.001) - mil);//m/s
+			target = auto_set.getTargetPos();
+
+			mil = (double)loop * 0.001;
+
+		}
+			//Print << auto_set.next_status();
+			//Print << next_point;
+			Print << U"x : " << target.x;
+			Print << U"y : " << target.y;
+			//Print << U"xS : " << targetSpeed.x;
+			//Print << U"yS : " << targetSpeed.y;
+			plotter1
+				.resize(Scene::Rect())
+				.plot(targetSpeed.x)//x
+				.maximum(10)
+				.minimum(-10)
+				.draw(Palette::Green)
+				.drawGrid();
+			plotter2
+				.resize(Scene::Rect())
+				.plot(targetSpeed.y)//y
+				.maximum(10)
+				.minimum(-10)
+				.draw(Palette::Yellow)
+				.drawGrid();
+			plotter3
+				.resize(Scene::Rect())
+				.plot(0/*auto_set.getTargetPos().x*/)//y
+				.maximum(10)
+				.minimum(-10)
+				.draw(Palette::Blue)
+				.drawGrid();
+			plotter4
+				.resize(Scene::Rect())
+				.plot(auto_set.getTargetPos().y)//y
+				.maximum(10)
+				.minimum(-10)
+				.draw(Palette::Red)
+				.drawGrid();
+		
+	}
+}
---
+++ TrapezoidalControlTest/TrapezoidalControlTest/Trapezoidal.h
@@ -0,0 +1,181 @@
+﻿#pragma once
+/*
+* 現状の課題点は次の順路に移行したときに、がたつきが見られるところ
+* 長さと旋回角の台形制御をして、ベジェ曲線の経路クラスと合わせて使用することを目的とした。
+*/
+# include <Siv3D.hpp> // OpenSiv3D v0.6.
+
+
+class MyTimer {
+	unsigned long times;
+	bool stopFlag;
+	bool resetFlag;
+	bool timerState;
+	unsigned long millis() const {
+		return (unsigned long)Time::GetMillisec();
+	}
+public:
+	MyTimer()
+		: times()
+		, stopFlag(false)
+		, resetFlag(false)
+		, timerState(false)
+	{}
+	/// @brief タイマー時間を返す
+	unsigned long getTime()const {
+		if (resetFlag)return 0;
+		if (stopFlag)return times;
+		return millis() - times;// ms
+	}
+	/// @brief タイマーの始動（何度呼び出してもかまわない）
+	void start() {
+		if (not timerState)
+			times = millis();
+		timerState = true;
+		stopFlag = false;
+		resetFlag = false;
+	}
+	/// @brief タイマー停止
+	void stop() {
+		timerState = false;
+		if (not stopFlag)
+			times = millis() - times;
+		stopFlag = true;
+	}
+	/// @brief タイマーのリセット
+	void reset() {
+		resetFlag = true;
+	}
+};
+
+class Trapezoidal {
+private:
+	double maxSpeed, limitSpeed;
+	double upTime, maxPowerTime, downTime;
+	double dir, acc;
+	bool calState, nextState;
+	Vec2 nowPos;
+	Vec2 targetPos;
+	Vec2 targetSpeed;
+	Vec2 oldTarget;
+	unsigned long oldMillis;
+	MyTimer myTimer;
+	unsigned long millis() const {
+		return (unsigned long)Time::GetMillisec();
+	}
+	double constrain(const double x, const double a, const double b) {
+		if (x < a)
+			return a;
+		else if (b < x)
+			return b;
+		else
+			return x;
+	}
+	double sq(double r) {
+		return r * r;
+	}
+
+	int select(double r) {
+		if (r >= Math::Pi * 1.5 && r < Math::Pi * 2) {
+			return -1;
+		}
+		else {
+			return 1;
+		}
+	}
+
+public:
+	Trapezoidal(double acc, double maxSpeed, Vec2 nowPos)
+		: acc(acc)
+		, maxSpeed(maxSpeed)
+		, nowPos(nowPos)
+		, limitSpeed()
+		, upTime()
+		, maxPowerTime()
+		, downTime()
+		, dir()
+		, calState(false)
+		, nextState(true)
+		, targetSpeed()
+		, oldTarget()
+		, oldMillis()
+		, targetPos()
+		, myTimer()
+	{
+	}
+
+	Vec2 operator()(Vec2 target,double starSpeed=0,double enSpeed=0){
+		return { 0,0 };
+	}
+
+	void update(Vec2 target, double startSpeed = 0, double endSpeed = 0) {
+		//  X=Vot+(1/2)*at^2;
+		if (!calState) {
+			const Vec2 distance = target - nowPos;//極座標でのrを求める
+			dir = atan2(distance.y, distance.x);//返り値はラジアン atan2は座標から角度を求める
+			// y:- x:+
+			startSpeed *= select(dir);
+			endSpeed *= select(dir);
+
+			upTime = (maxSpeed - startSpeed) / acc; //最高速度までの加速にかかる時間 /s
+			downTime = (maxSpeed - endSpeed) / acc; //減速にかかる時間 /s
+			double L1 = startSpeed * upTime + acc * sq(upTime) * 0.5; //加速時における移動距離 /m
+			double L3 = endSpeed * downTime + acc * sq(downTime) * 0.5; //減速時における移動距離/m
+			if (abs(L1 + L3) > abs(distance.length())) {//台形ができなくなり、三角形になるときの制御
+				limitSpeed = 2.00 * acc * distance.length() * 0.5 + sq(startSpeed);
+				limitSpeed = sqrt(limitSpeed);
+				upTime = (limitSpeed - startSpeed) / acc;
+				downTime = (limitSpeed - endSpeed) / acc;
+				maxPowerTime = 0.0;
+			}
+			else {//台形が作れる場合の制御 
+				maxPowerTime = (distance.length() - L1 - L3) / maxSpeed; //最高速度での移動時間 /s
+				limitSpeed = maxSpeed;
+			}
+			calState = true;
+		}
+		if (calState)
+			myTimer.start();//タイマーを開始する←何度呼び出しても一度だけ実行される
+
+		double t = myTimer.getTime() * 0.001;  //s
+		double ut = constrain(t, 0.00, upTime);
+		double dt = constrain(t - (upTime + maxPowerTime), 0.00, downTime);
+		double target_ = acc * sq(ut) * 0.50 + startSpeed * ut
+			+ maxSpeed * constrain(t - upTime, 0.00, maxPowerTime)
+			+ (-acc * sq(dt) * 0.50) + limitSpeed * dt;
+		//座標の更新をするところ
+		targetPos.x = target_ * cos(dir) + nowPos.x;
+		targetPos.y = target_ * sin(dir) + nowPos.y;
+
+		if (upTime + downTime + maxPowerTime <= t) {
+			nowPos.x = targetPos.x;//一つの経路を巡行し終えた時の座標の情報を保持
+			nowPos.y = targetPos.y;
+			myTimer.stop();
+			myTimer.reset();
+			calState = false;
+			nextState = true;
+		}
+
+		// difdistance / diftime
+		targetSpeed.y = (getTargetPos().y - oldTarget.y) / (((double)millis() * 0.001) - oldMillis);  //m/s
+		targetSpeed.x = (getTargetPos().x - oldTarget.x) / (((double)millis() * 0.001) - oldMillis);  //m/s
+		oldMillis = millis() * 0.001;
+		oldTarget = getTargetPos();
+	}
+
+	/// @brief 実行状態を返す
+	/// @return 今回の経路を巡行し終えたらtrue
+	bool next_status() {
+		return nextState;
+	}
+	void set_next_status() {//次の経路をセットしたら必ず行う
+		nextState = false;
+	}
+	Vec2 getNowPos() {
+		return nowPos;
+	}
+	Vec2 getTargetPos() {
+		return targetPos;
+	}
+};
+
