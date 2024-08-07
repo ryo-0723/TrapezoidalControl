5f9641e3791b278f71941555614edd3493f5dde0
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2024-08-07 12:05:33 +0900

    プログラム整理

--- TrapezoidalControlTest/TrapezoidalControlTest/Trapezoidal.h
+++ TrapezoidalControlTest/TrapezoidalControlTest/Trapezoidal.h
@@ -52,14 +52,10 @@ private:
 	double acc , maxSpeed, limitSpeed;
 	bool nextState,dir ;
 	double nowPos, nowSpeed;
-	double targetPos,oldTarget;
+	double oldTarget;
 	double startSpeed, endSpeed;
-	bool calState;
-	double all;
-	double dis;
 	double nextNowPos, nextNowSpeed;
 	MyTimer myTimer;
-
 
 	unsigned long millis() const {
 		return (unsigned long)Time::GetMillisec();
@@ -92,24 +88,15 @@ public:
 		, nextState(true)
 		, dir()
 		, nowSpeed()
-		, targetPos(nowPos)
 		, oldTarget(nowPos)
 		, startSpeed()
 		, endSpeed()
-		, calState(false)
-		, all()
-		, dis()
-		,nextNowPos()
-		,nextNowSpeed()
+		, nextNowPos(nowPos)
+		, nextNowSpeed()
 		, myTimer()
 	{
 	}
-
 	//入力はs単位で計算時はms単位にする
-
-	//double operator()(double target, double starSpeed = 0, double enSpeed = 0) {
-	//	return 0;
-	//}
 
 	/// @brief 目標値を計算する
 	/// @param target mm 目標値
@@ -131,89 +118,58 @@ public:
 					const double distance = target - nowPos;//距離を求める
 					dir = (distance > 0.0 ? 1 : 0);//増減の方向を決める
 
-					if (!dir) {//マイナス方向への進行の場合
-						endSpeed = abs(endSpeed) * -1;
-						maxSpeed = abs(maxSpeed) * -1;
-						acc = abs(acc) * -1;
-					}
-					else {
-						endSpeed = abs(endSpeed);
-						maxSpeed = abs(maxSpeed);
-						acc = abs(acc);
-					}
-
+					endSpeed = abs(endSpeed) * (dir ? 1 : -1);
+					maxSpeed = abs(maxSpeed) * (dir ? 1 : -1);
+					acc = abs(acc) * (dir ? 1 : -1);
 
 					upTime = (maxSpeed - startSpeed) / acc; //最高速度までの加速にかかる時間 ms
 					downTime = (maxSpeed - endSpeed) / acc; //減速にかかる時間 ms
-					if (upTime < 0.0) {
+					if (upTime < 0.0)//今と逆に進みたいときに減速分を増加させるため
 						downTime -= upTime;
-					}
 					
 					const double L1 = startSpeed * upTime + acc * sq(upTime) * 0.5; //加速時における移動距離 mm
 					const double L3 = endSpeed * downTime + acc * sq(downTime) * 0.5; //減速時における移動距離 mm
 					if (abs(L1) + abs(L3) > abs(distance)) { //台形ができなくなり、三角形になるときの制御
-						limitSpeed = 2.00 * acc * distance * 0.5 + sq(startSpeed);//+限定
-						limitSpeed = sqrt(limitSpeed) * (dir ? 1 : -1);//必ず＋になるため、向きによって＋ーを変える
-						if (abs(limitSpeed) < abs(startSpeed)) {
+						limitSpeed = 2.00 * acc * distance * 0.5 + sq(startSpeed);
+						limitSpeed = sqrt(limitSpeed) * (dir ? 1 : -1);//必ず＋になる
+						if (abs(limitSpeed) < abs(startSpeed))
 							limitSpeed = startSpeed;
-						}
-						upTime = (limitSpeed - startSpeed) / acc;//必ず0以上になる保証なし
+						upTime = (limitSpeed - startSpeed) / acc;
 						downTime = (limitSpeed - endSpeed) / acc;
-						maxPowerTime = 0.0;
-						//limitSpeed,upTimeがマイナスになるかもしれない？
+						maxPowerTime = 0.0;//limitSpeed,upTimeがマイナスになることもある
 					}
 					else {//台形が作れる場合の制御
 						maxPowerTime = (distance - L1 - L3) / maxSpeed; //最高速度での移動時間 ms
 						limitSpeed = maxSpeed;
 					}
-
-					all = acc * sq(upTime) * 0.50 + startSpeed * upTime
-						+ maxSpeed * maxPowerTime
-						+ (-acc * sq(downTime) * 0.50) + limitSpeed * downTime + nowPos;
-
-					dis = distance + nowPos;
-					//distanceと計算した距離が同じじゃない
-					//何度計算しなおしても同じっぽい
-					//startSpeedが原因？
-					//一周期遅延をいれてみる？
-					//三角になるときにずれてるっぽい
-					//目標値を変えた際に確定で行き過ぎる場合があり、その場合に逆に進んでいないから目標値がずれてる？
+					//目標値がずれることがあった↓
+					//目標値を変えた際の速度によって確定で行き過ぎる場合があり、
+					//その場合に逆に進んでいないから目標値がずれてる
 			}
 	
-
-			double t = myTimer.getTime();  //ms
-			double ut = constrain(t, 0.00, upTime);
-			double mt = constrain(t - upTime, 0.00, maxPowerTime);
-			double dt = constrain(t - (upTime + maxPowerTime), 0.00, downTime);
+			double ut = constrain(myTimer.getTime(), 0.00, upTime);
+			double mt = constrain(myTimer.getTime() - upTime, 0.00, maxPowerTime);
+			double dt = constrain(myTimer.getTime() - (upTime + maxPowerTime), 0.00, downTime);
 			double targets = acc * sq(ut) * 0.50 + startSpeed * ut
 				+ maxSpeed * mt
 				+ (-acc * sq(dt) * 0.50) + limitSpeed * dt;
 
-			if (!nextState) {
-				nowSpeed = startSpeed + acc * ut + -acc * dt;
-				targetPos = targets + nowPos;
+			if (!nextState) {//経路を巡行してないときだけ更新する
+				nextNowSpeed = startSpeed + acc * ut + -acc * dt;
+				nextNowPos = targets + nowPos;
 			}
-			nextNowPos = targetPos;
-			nextNowSpeed = nowSpeed;
-
-			if (upTime + downTime + maxPowerTime <= t) {
-				nowPos = targetPos;//一つの経路を巡行し終えた時の座標の情報を保持
+			if (upTime + downTime + maxPowerTime <= myTimer.getTime()) {
+				nowPos = nextNowPos;//一つの経路を巡行し終えた時の座標の情報を保持
 				myTimer.stop();
 				myTimer.reset();
 				nextState = true;
-				oldTarget = nowPos;
+				oldTarget = nowPos;//行き過ぎた場合にもう一度計算するようにする
 			}
-
-			Print <<U"oldTarget:" << oldTarget;
-			Print <<U"nowTarget:" << all;
-			Print <<U"distance :" << dis;
-			Print << U"INPUT   :" << target;
 	}
 
-	double getTime() {
+	double getTime() const{
 		return myTimer.getTime();
 	}
-
 	/// @brief 実行状態を取得する
 	/// @return セットされている経路を巡行し終えたらtrue
 	bool status() const{
@@ -227,7 +183,7 @@ public:
 	/// @brief 台形制御の現在座標を取得する
 	/// @return 計算座標 mm
 	double getTargetPos() const{
-		return targetPos;
+		return nextNowPos;
 	}
 	/// @brief 現在速度を取得する
 	/// @return 現在速度 m/s
@@ -235,4 +191,3 @@ public:
 		return nowSpeed * 1000;
 	}
 };
-
